c0141e7544f181de5eb21d8c2c413d6f152fefe0
sanjoy <sanjoylab66@gmail.com>
2023-09-01 10:11:35 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <p className=' text-center mt-10 text-2xl'>Web design, Development, Maintaining and IT service company.</p>
+      <a className='text-center block my-10 text-2xl underline hover:underline-offset-4' href="https://www.youtube.com/@SanjoyLab">YouTube</a>
+      <p className='my-14 text-center text-red-500 text-2xl'>Developing...</p>
+    </div>
+  )
+}
